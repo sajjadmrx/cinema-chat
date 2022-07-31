37b6b7811901ff7b47ba1d4b923f0ecf0e59845b
sajjadmrx <sajjadzibaee102@gmail.com>
2022-07-31 18:55:57 +0430

    use prisma types

--- src/shared/interfaces/room.interface.ts
+++ src/shared/interfaces/room.interface.ts
@@ -1,8 +1,9 @@
-import { Room as _Room } from '@prisma/client';
+import { Prisma, Room as _Room } from '@prisma/client';
 
 export interface Room extends _Room { }
 
 
-export interface RoomCreateInput extends Omit<Room, 'createdAt' | 'updatedAt' | 'id' | 'roomId'> { }
-
-export interface RoomUpdateInput extends Partial<RoomCreateInput> { }
+export interface RoomCreateInput extends Omit<Prisma.RoomCreateInput, 'roomId' | 'owner'> {
+    ownerId: number
+}
+export interface RoomUpdateInput extends Prisma.RoomUpdateInput { }
