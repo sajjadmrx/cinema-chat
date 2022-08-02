1ed6e893737cfa488dc44a4cedd0e661f1f95d81
sajjadmrx <sajjadzibaee102@gmail.com>
2022-08-02 11:25:52 +0430

    create first  member

--- src/modules/rooms/rooms.repository.ts
+++ src/modules/rooms/rooms.repository.ts
@@ -1,36 +1,46 @@
 import { Injectable } from '@nestjs/common';
-import { Room, RoomCreateInput, RoomUpdateInput } from 'src/shared/interfaces/room.interface';
+import {
+  Room,
+  RoomCreateInput,
+  RoomUpdateInput,
+} from 'src/shared/interfaces/room.interface';
 import { getRandomNumber } from 'src/shared/utils/uuid.util';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class RoomsRepository {
-  constructor(private db: PrismaService) { }
+  constructor(private db: PrismaService) {}
 
   async insert(input: RoomCreateInput): Promise<Room> {
     return this.db.room.create({
       data: {
         ...input,
         roomId: getRandomNumber(8),
-      }
-    })
+        Members: {
+          create: [
+            {
+              userId: input.ownerId,
+            },
+          ],
+        },
+      },
+    });
   }
 
   async getById(id: number): Promise<Room | null> {
     return this.db.room.findUnique({
       where: {
-        roomId: id
-      }
-    })
+        roomId: id,
+      },
+    });
   }
 
   async updateById(id: number, input: RoomUpdateInput): Promise<Room> {
     return this.db.room.update({
       where: {
-        roomId: id
+        roomId: id,
       },
-      data: input
-    })
+      data: input,
+    });
   }
-
 }
--- src/shared/interfaces/member.interface.ts
+++ src/shared/interfaces/member.interface.ts
@@ -1,16 +1,15 @@
-import { Member as _Member, Prisma } from "@prisma/client"
+import { Member as _Member, Prisma } from '@prisma/client';
 import { Room } from 'src/shared/interfaces/room.interface';
 
-
-export interface Member extends _Member { }
-
+export interface Member extends _Member {}
 
 export interface MemberWithRoom extends Member {
-    room: Room
+  room: Room;
 }
 
-export interface MemberCreateInput extends Omit<Prisma.MemberCreateInput, | "room" | "invite" | "user"> {
-    roomId: number
-    userId: number
-    inviteId: number
+export interface MemberCreateInput
+  extends Omit<Prisma.MemberCreateInput, 'room' | 'invite' | 'user'> {
+  roomId: number;
+  userId: number;
+  inviteId?: number | null;
 }
