7dcb1bd52850201b5ea044682846d46ecb662907
Alireza Askarpour <askarpourdev@gmail.com>
2023-04-17 16:44:36 -0400

    feat: add tailwind css

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="fa-IR" dir="rtl">
       <Head />
       <body>
         <Main />
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["shabnam", "rancho", "poppins"]
+    }
+  },
+  plugins: [],
+}
