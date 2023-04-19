08c966f33314b4c31ad9af412b7b24fa41ae33e9
ali.zabetpour <ali_0_zabetpoor@ymail.com>
2023-04-19 21:58:00 +0330

    front/ali add videojs package add videojs player to main page add videojs player component add mediaqueries mixin change global css add media sizes to theme change name theme to _theme

--- frontend/src/components/MainPage/index.tsx
+++ frontend/src/components/MainPage/index.tsx
@@ -1,12 +1,41 @@
+import { useRef } from "react";
 import Chatbox from "@/components/Chatbox";
 import VideoPlayer from "@/components/VideoPlayer";
 import styles from "./style.module.scss";
 
 const MainPage = () => {
+  const playerRef = useRef(null);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: "http://193.36.85.124:4000/stream/hls/cat/cat_480_hls.m3u8",
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+
   return (
     <div className={styles.mainPage}>
       <Chatbox />
-      <VideoPlayer />
+      <VideoPlayer options={videoJsOptions} onReady={handlePlayerReady} />
     </div>
   );
 };
