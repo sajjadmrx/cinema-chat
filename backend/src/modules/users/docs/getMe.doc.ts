1997da527dc8c0c73000b7c4f1b81b82b0f5b393
sajjadmrx <sajjadzibaee102@gmail.com>
2023-04-19 19:18:55 +0430

    get me

---
+++ backend/src/modules/users/docs/getMe.doc.ts
@@ -0,0 +1,26 @@
+import { applyDecorators, HttpStatus } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+
+export function ApiGetMe() {
+  return applyDecorators(
+    ApiOperation({
+      summary: 'get current user data',
+    }),
+    ApiOkResponse({
+      schema: {
+        example: {
+          statusCode: HttpStatus.OK,
+          data: {
+            id: '6309b988f06fc80bca9e1b22',
+            userId: 692869485481,
+            email: 'fake@gmail.com',
+            username: 'sajjadmrx',
+            permissions: ['ADMINISTRATOR'],
+            createdAt: '2022-08-27T06:28:24.870Z',
+            updatedAt: '2022-09-07T08:41:05.801Z',
+          },
+        },
+      },
+    }),
+  );
+}
