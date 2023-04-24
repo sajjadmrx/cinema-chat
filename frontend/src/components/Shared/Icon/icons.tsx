d417c6b77fd0ca1e50d53869b7c0ff215fd3195f
Alireza Askarpour <askarpourdev@gmail.com>
2023-04-24 16:27:48 -0400

    faet: implement rooms section

--- frontend/src/components/Shared/Icon/icons.tsx
+++ frontend/src/components/Shared/Icon/icons.tsx
@@ -3,6 +3,7 @@ import Lock from "./Icons/Lock"
 import UnLock from "./Icons/UnLock"
 import Mail from "./Icons/Mail"
 import Eye from "./Icons/Eye"
+import Radar from "./Icons/Radar"
 import EyeSlash from "./Icons/EyeSlash"
 
 export const icons = {
@@ -11,6 +12,7 @@ export const icons = {
   unlock: UnLock,
   mail: Mail,
   eye: Eye,
+  radar: Radar,
   "eye-slash": EyeSlash,
 }
 
