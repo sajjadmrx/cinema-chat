0cadd19d62727cc31f9cede158803edca86f1e06
ali.zabetpour <ali_0_zabetpoor@ymail.com>
2023-04-27 19:47:55 +0330

    add video player

--- frontend/src/pages/Room.tsx
+++ frontend/src/pages/Room.tsx
@@ -1,10 +1,38 @@
-import React from "react"
+import { useRef } from "react"
 import Layout from "../components/Layout"
 import Navbar from "../components/Layout/Navbar"
+import VideoPlayer from "../components/VideoPlayer"
 import Chats from "../views/Room/Chats"
 import Members from "../views/Room/Members"
 
 const Room = () => {
+  const playerRef = useRef(null)
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: "http://193.36.85.124:4000/stream/hls/cat/cat_480_hls.m3u8",
+        type: "application/x-mpegURL",
+      },
+    ],
+  }
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting")
+    })
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose")
+    })
+  }
   return (
     <Layout>
       <Navbar />
@@ -20,7 +48,7 @@ const Room = () => {
             <h4 className="ml-3 -mb-1.5 font-bold">Call of Duty: World War II</h4>
           </div>
           <div className="bg-gray-200 w-full h-96 rounded-2xl mt-6 grid place-items-center border border-gray-300">
-            Video Player
+            <VideoPlayer options={videoJsOptions} onReady={handlePlayerReady} />
           </div>
         </div>
         <Chats />
