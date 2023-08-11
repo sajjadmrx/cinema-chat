074c8f2a60442c086c7be78a654353a18bc33b00
Mani Ramezanpour <maniramznpoor2007@gmail.com>
2023-08-11 23:10:40 +0330

    change room chat style

--- frontend/src/pages/single-room/Room.tsx
+++ frontend/src/pages/single-room/Room.tsx
@@ -1,19 +1,16 @@
 import { Room } from "@interfaces/schemas/Room.interface"
 import { MemberWithRoom } from "@interfaces/schemas/member.interface"
 import React, { useEffect, useRef, useState } from "react"
-import { Button, Input } from "react-daisyui"
 import { useNavigate, useParams } from "react-router-dom"
 import Layout from "../../components/Layout"
 import Navbar from "../../components/Layout/Navbar"
-import MediaPlayer from "../../components/VideoPlayer"
 import { useAuth } from "../../context/auth/AuthProvider"
 import { socketContext } from "../../context/socket/socketContext"
 import { socket } from "../../hooks/useSocket"
 import { getMemberByMemberId } from "../../services/members.service"
 import ChatsComponent from "../../views/Room/Chats"
-import MembersComponent from "../../views/Room/Members"
 
-let currentMediaId: number | null = null
+const currentMediaId: number | null = null
 export const RoomPage = (): any => {
   const playerRef = useRef(null)
   const navigate = useNavigate()
@@ -122,24 +119,24 @@ export const RoomPage = (): any => {
         }}
       >
         <section className="flex h-[calc(100vh-72px)] lg:flex-row flex-col">
-          {socket.connected && (
+          {/* {socket.connected && (
             <MembersComponent
               roomId={Number(params.id)}
               showMembers={showMembers}
               setShowMembers={setShowMembers}
             />
-          )}
-          <div className="flex-1 px-6 py-5">
+          )} */}
+          {/* <div className="flex-1 px-6 py-5">
             <div className={"flex flex-row gap-40"}>
-              <div className="flex items-center pb-5 border-b border-b-gray-100">
+              {/* <div className="flex items-center pb-5 border-b border-b-gray-100">
                 <img
                   className="w-16 h-16 rounded-full border-[6px] border-gray-200"
                   src="https://xsgames.co/randomusers/avatar.php?g=pixel"
                   alt={""}
                 />
                 <h4 className="ml-3 -mb-1.5 font-bold">{room?.name}</h4>
-              </div>
-              <div className={""}>
+              </div> */}
+          {/* <div className={""}>  
                 Play Media
                 {member?.permissions.includes("ADMINISTRATOR") ? (
                   <div className={"flex flex-row gap-11"}>
@@ -159,18 +156,18 @@ export const RoomPage = (): any => {
                   </div>
                 )}
               </div>
-            </div>
+            </div> */}
 
-            {/*<div className="grid w-full mt-6 bg-gray-200 border border-gray-300 h-96 rounded-2xl place-items-center">*/}
-            <div>
-              <MediaPlayer
-                src={videoJsOptions?.src}
-                roomId={Number(params.id)}
-                paused={videoJsOptions.paused}
-                currentTime={videoJsOptions.currentTime}
-              />
-            </div>
-          </div>
+          {/*<div className="grid w-full mt-6 bg-gray-200 border border-gray-300 h-96 rounded-2xl place-items-center">*/}
+          {/* <div>
+            <MediaPlayer
+              src={videoJsOptions?.src}
+              roomId={Number(params.id)}
+              paused={videoJsOptions.paused}
+              currentTime={videoJsOptions.currentTime}
+            />
+          </div> */}
+          {/* </div> */}
           {socket.connected && <ChatsComponent setShowMembers={setShowMembers} />}
         </section>
       </socketContext.Provider>
