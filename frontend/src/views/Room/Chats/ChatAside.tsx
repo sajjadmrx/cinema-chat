564d3160b848d545c16ede88d3b2ec8f72611d9a
Mani Ramezanpour <maniramznpoor2007@gmail.com>
2023-08-18 23:29:59 +0330

    change some style

---
+++ frontend/src/views/Room/Chats/ChatAside.tsx
@@ -0,0 +1,75 @@
+import { Dialog, Transition } from "@headlessui/react"
+import React, { Fragment, useState } from "react"
+// import { XMarkIcon } from "@heroicons/react/24/outline"
+import { AiOutlineClose } from "react-icons/ai"
+export default function ChatAside() {
+  const [open, setOpen] = useState(true)
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10 lg:hidden" onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-in-out duration-500"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in-out duration-500"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="fixed inset-y-0 right-0 flex max-w-full pl-10 pointer-events-none">
+              <Transition.Child
+                as={Fragment}
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+              >
+                <Dialog.Panel className="relative w-screen max-w-md pointer-events-auto">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-500"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-500"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="absolute top-0 left-0 flex pt-4 pr-2 -ml-8 sm:-ml-10 sm:pr-4">
+                      <button
+                        type="button"
+                        className="relative text-gray-300 rounded-md hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                        onClick={() => setOpen(false)}
+                      >
+                        <span className="absolute -inset-2.5" />
+                        <span className="sr-only">Close panel</span>
+                        <AiOutlineClose className="w-6 h-6" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </Transition.Child>
+                  <div className="flex flex-col h-full py-6 overflow-y-scroll bg-white shadow-xl">
+                    <div className="px-4 sm:px-6">
+                      <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
+                        Panel title
+                      </Dialog.Title>
+                    </div>
+                    <div className="relative flex-1 px-4 mt-6 sm:px-6">
+                      {/* Your content */}
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
+}
--- frontend/src/views/Room/Chats/index.tsx
+++ frontend/src/views/Room/Chats/index.tsx
@@ -3,13 +3,15 @@ import Picker from "@emoji-mart/react"
 import { Message } from "@interfaces/schemas/message.interface"
 import React, { useContext, useEffect, useRef, useState } from "react"
 import { AiOutlineUser } from "react-icons/ai"
-import { BsEmojiSmile } from "react-icons/bs"
+import { BsEmojiSmile, BsFillChatLeftDotsFill } from "react-icons/bs"
 import { IoSendSharp } from "react-icons/io5"
 import { useParams } from "react-router-dom"
 import { useAuth } from "../../../context/auth/AuthProvider"
 import { SocketContext, socketContext } from "../../../context/socket/socketContext"
 import { socket } from "../../../hooks/useSocket"
 import { fetchMessages } from "../../../services/message.service"
+// import ChatAside from "./ChatAside"
+import ChatAside from "./ChatAside"
 import DropdownMenu from "./DropdownMenu"
 interface Prop {
   setShowMembers: any
@@ -25,7 +27,7 @@ const ChatsComponent = ({ setShowMembers }: Prop) => {
   const { user } = useAuth()
   const params = useParams()
   const scrollRef = useRef<any>()
-
+  const [chatSidebar, setchatSide] = useState<boolean>(false)
   useEffect(() => {
     socket.on("CREATE_MESSAGE", (message: Message) => {
       if (message.roomId == Number(params.id)) {
@@ -57,86 +59,95 @@ const ChatsComponent = ({ setShowMembers }: Prop) => {
   }
 
   return (
-    <section className="hidden w-2/5 px-6 py-5 border-l-2 border-gray-300 lg:block lg:h-full bg-dark lg:border-l rounded-xl">
-      <div className="flex flex-col text-white">
-        <div className="flex items-center justify-between pb-3 mb-5 border-b border-gray-100">
-          {" "}
-          <h2 className="text-lg font-semibold">Chat</h2>
-          <AiOutlineUser
-            onClick={() => setShowMembers(true)}
-            className="lg:hidden"
-            size={24}
-          />
-        </div>
-      </div>
-      <div
-        className="space-y-5 h-[calc(100%-129px)] text-white overflow-y-auto"
-        ref={scrollRef}
-      >
-        {messages.map((item, index) => (
-          <div key={index} className="mr-2">
-            <div className="flex">
-              <img
-                className="border border-gray-200 rounded-full w-9"
-                src="/assets/images/avatar.jpg"
-                alt={"avatar"}
-              />
-              <div className="flex items-center justify-between w-full">
-                <h4 className="ml-3 ">
-                  {item.authorId == user?.userId}
-                  {item.author?.nickname || item.author.user.username}
-                </h4>
-                <DropdownMenu />
-              </div>
-            </div>
-
-            <div
-              className="px-4 py-2 ml-12 text-sm bg-gray-100 border rounded-tl-none text-dark rounded-2xl"
-              style={{ wordBreak: "break-word" }}
-            >
-              {item.content}
-              <div className="flex justify-end mt-1 text-xs text-gray-500">
-                <i>
-                  {new Date(item.createdAt).getHours().toLocaleString()}:
-                  {new Date(item.createdAt).getMinutes().toLocaleString()}{" "}
-                  {new Date(item.createdAt).getHours() < 14 ? "AM" : "PM"}
-                </i>
-              </div>
-            </div>
+    <>
+      <button onClick={() => setchatSide(true)}>
+        <BsFillChatLeftDotsFill className="text-2xl text-white lg:hidden" />
+      </button>
+      <section className="hidden w-2/5 px-6 py-5 border-l-2 border-gray-300 lg:block lg:h-full bg-dark lg:border-l rounded-xl">
+        <div className="flex flex-col text-white">
+          <div className="flex items-center justify-between pb-3 mb-5 border-b border-gray-100">
+            {" "}
+            <h2 className="text-lg font-semibold">Chat</h2>
+            <AiOutlineUser
+              onClick={() => setShowMembers(true)}
+              className="lg:hidden"
+              size={24}
+            />
           </div>
-        ))}
-      </div>
-      <div className="w-full h-12 mt-5 overflow-hidden border rounded-full">
-        <div className="relative w-full h-full" dir={"auto"}>
-          <textarea
-            dir={"auto"}
-            placeholder="Write a message..."
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            className="absolute border text-black border-primary   top-0 left-0 flex items-center justify-center w-full h-full !pt-3.5 pl-4 text-sm bg-white rounded-full focus:outline-none focus:border-gray-500 "
-            onClick={() => setShowPicker(false)}
-          />
-          <button
-            onClick={() => setShowPicker(!showPicker)}
-            className="absolute z-10 -translate-y-1/2 top-1/2 right-10"
-          >
-            <span role="img" aria-label="smiley-face">
-              <BsEmojiSmile className={"text-black text-2xl"} />
-            </span>
-          </button>
-          <button
-            className="absolute z-10 -translate-y-1/2 top-1/2 right-2"
-            onClick={sendMsg}
-          >
-            <IoSendSharp className={"text-primary text-2xl"} />
-          </button>
         </div>
-        <div className="absolute" style={{ bottom: 83, right: 26 }} hidden={!showPicker}>
-          <Picker data={data} onEmojiSelect={handleEmojiSelect} />
+        <div
+          className="space-y-5 h-[calc(100%-129px)] text-white overflow-y-auto"
+          ref={scrollRef}
+        >
+          {messages.map((item, index) => (
+            <div key={index} className="mr-2">
+              <div className="flex">
+                <img
+                  className="border border-gray-200 rounded-full w-9"
+                  src="/assets/images/avatar.jpg"
+                  alt={"avatar"}
+                />
+                <div className="flex items-center justify-between w-full">
+                  <h4 className="ml-3 ">
+                    {item.authorId == user?.userId}
+                    {item.author?.nickname || item.author.user.username}
+                  </h4>
+                  <DropdownMenu />
+                </div>
+              </div>
+
+              <div
+                className="px-4 py-2 ml-12 text-sm bg-gray-100 border rounded-tl-none text-dark rounded-2xl"
+                style={{ wordBreak: "break-word" }}
+              >
+                {item.content}
+                <div className="flex justify-end mt-1 text-xs text-gray-500">
+                  <i>
+                    {new Date(item.createdAt).getHours().toLocaleString()}:
+                    {new Date(item.createdAt).getMinutes().toLocaleString()}{" "}
+                    {new Date(item.createdAt).getHours() < 14 ? "AM" : "PM"}
+                  </i>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      </div>
-    </section>
+        <div className="w-full h-12 mt-5 overflow-hidden border rounded-full">
+          <div className="relative w-full h-full" dir={"auto"}>
+            <textarea
+              dir={"auto"}
+              placeholder="Write a message..."
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="absolute border text-black border-primary   top-0 left-0 flex items-center justify-center w-full h-full !pt-3.5 pl-4 text-sm bg-white rounded-full focus:outline-none focus:border-gray-500 "
+              onClick={() => setShowPicker(false)}
+            />
+            <button
+              onClick={() => setShowPicker(!showPicker)}
+              className="absolute z-10 -translate-y-1/2 top-1/2 right-10"
+            >
+              <span role="img" aria-label="smiley-face">
+                <BsEmojiSmile className={"text-black text-2xl"} />
+              </span>
+            </button>
+            <button
+              className="absolute z-10 -translate-y-1/2 top-1/2 right-2"
+              onClick={sendMsg}
+            >
+              <IoSendSharp className={"text-primary text-2xl"} />
+            </button>
+          </div>
+          <div
+            className="absolute"
+            style={{ bottom: 83, right: 26 }}
+            hidden={!showPicker}
+          >
+            <Picker data={data} onEmojiSelect={handleEmojiSelect} />
+          </div>
+        </div>
+      </section>
+      {chatSidebar ? <ChatAside /> : ""}
+    </>
   )
 }
-
 export default ChatsComponent
